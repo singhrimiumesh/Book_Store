1f13d98f2bb7e4a6f0e067d57b603c1e4d4951bb
rimi <singhrimiumesh@gmail.com>
2024-01-30 17:46:17 +0530

    Sixth Commit

--- mern-client/src/dashboard/EditBooks.jsx
+++ mern-client/src/dashboard/EditBooks.jsx
@@ -1,8 +1,178 @@
+import { Button, Label, TextInput, Textarea } from "flowbite-react";
+import { useLoaderData, useParams } from "react-router-dom"
+import { useState } from "react";
 
 const EditBooks = () => {
+  const {id} = useParams();
+  const {bookTitle, authorName, imageURL, category, bookDescription, bookPDFURL} = useLoaderData();
+  
+  const bookCategories = [
+    "Programming",
+    "Business",
+    "DSA",
+    "SE",
+    "DBMS",
+    "CN",
+    "OS",
+    "AI",
+    "ML",
+    "UX/UI",
+    "Cybersecurity",
+    "Mathematics",
+  ];
+  const [selectedBookCategory, setSelectedBookCategory] = useState(
+    bookCategories[0]
+  );
+
+  const handleChangeSelectValue = (event) => {
+    // console.log(event.target.value);
+    setSelectedBookCategory(event.target.value);
+  };
+  // handle book submission
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const form = event.target;
+
+    const bookTitle = form.bookTitle.value;
+    const authorName = form.authorName.value;
+    const imageURL = form.imageURL.value;
+    const category = form.categoryName.value;
+    const bookDescription = form.bookDescription.value;
+    const bookPDFURL = form.bookPDFURL.value;
+
+    const updateBookObj = {
+      bookTitle,
+      authorName,
+      imageURL,
+      category,
+      bookDescription,
+      bookPDFURL,
+    };
+    // console.log(bookObj);
+    //update book
+    fetch(`http://localhost:5000/book/${id}`, {
+      method: "PATCH", 
+      headers:{
+        "Content-Type":"application/json"},
+        body:JSON.stringify(updateBookObj)
+    }).then(res =>  res.json()).then(data => {
+      alert("Book Updated Successfully!")
+  })
+  };
+
   return (
-    <div>EditBooks</div>
-  )
+    <div className="px-4 my-12">
+      <h2 className="mb-8 text-3xl font-bold">Update the Book data</h2>
+
+      <form
+        onSubmit={handleUpdate}
+        className="flex lg:w-[1180px] flex-col flex-wrap gap-4"
+      >
+        {/* First Row */}
+        <div className="flex gap-8">
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="bookTitle" value="Book Title" />
+            </div>
+            <TextInput
+              id="bookTitle"
+              name="bookTitle"
+              type="text"
+              placeholder="Book name"
+              required
+              defaultValue={bookTitle}
+            />
+          </div>
+          {/* authorName */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="authorName" value="Author Name" />
+            </div>
+            <TextInput
+              id="authorName"
+              name="authorName"
+              type="text"
+              placeholder="Author name"
+              required
+              defaultValue={authorName}
+            />
+          </div>
+        </div>
+
+        {/* Second Row */}
+        <div className="flex gap-8">
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="imageURL" value="Book Image URL" />
+            </div>
+            <TextInput
+              id="imageURL"
+              name="imageURL"
+              type="text"
+              placeholder="Book image URL"
+              required
+              defaultValue={imageURL}
+            />
+          </div>
+
+          {/* Category */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="inputState" value="Book Category" />
+            </div>
+            <select
+              id="inputState"
+              name="categoryName"
+              className="w-full rounded"
+              value={selectedBookCategory}
+              onChange={handleChangeSelectValue}
+            >
+              {bookCategories.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        {/* Book Description */}
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="bookDescription" value="Book Description" />
+          </div>
+          <Textarea
+            id="bookDescription"
+            type="text"
+            name="bookDescription"
+            placeholder="Write a book description..."
+            rows={6}
+            className="w-full"
+            required
+            defaultValue={bookDescription}
+          />
+        </div>
+
+        {/* Book PDF Link */}
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="bookPDFURL" value="Book PDF URL" />
+          </div>
+          <TextInput
+            id="bookPDFURL"
+            name="bookPDFURL"
+            type="text"
+            placeholder="Book PDF URL"
+            required
+            defaultValue={bookPDFURL}
+          />
+        </div>
+        <Button type="submit" className="mt-5">
+          Upload Book
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default EditBooks
--- mern-client/src/dashboard/ManageBooks.jsx
+++ mern-client/src/dashboard/ManageBooks.jsx
@@ -1,7 +1,64 @@
+import { Table } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom"
 
 const ManageBooks = () => {
+  const [allBooks, setAllBooks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => setAllBooks(data));
+  }, [])
+  // delete a book
+  const handleDelete = (id) => {
+      console.log(id);
+      fetch(`http://localhost:5000/book/${id}`,
+      {
+        method:"DELETE",
+      }).then(res => res.json()).then(data => {
+        alert("Book is Deleted Successfully!")
+        location.reload();
+        //setAllBooks(data);
+    })
+  }
+
   return (
-    <div>ManageBooks</div>
+    <div className="px-4 my-12">
+    <h2 className="mb-8 text-3xl font-bold">Manage Books</h2>
+    
+    {/* Table for book data */}
+    <Table className='lg:w-[1180px]'>
+        <Table.Head>
+          <Table.HeadCell>No.</Table.HeadCell>
+          <Table.HeadCell>Book name</Table.HeadCell>
+          <Table.HeadCell>Author Name</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>
+            <span>Edit/Manage</span>
+          </Table.HeadCell>
+        </Table.Head>
+        {
+         allBooks.map((book, index) => <Table.Body className="divide-y" key={book._id}>
+                      <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {index + 1}
+            </Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {book.bookTitle}
+            </Table.Cell>
+            <Table.Cell>{book.authorName}</Table.Cell>
+            <Table.Cell>{book.category}</Table.Cell>
+            <Table.Cell>
+              <Link href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 mr-5"
+              to= {`/admin/dashboard/edit-books/${book._id}`}>
+                Edit
+              </Link>
+              <button onClick={() => handleDelete(book._id)} className='bg-red-600  px-4 py-1 font-semibold text-white rounded-sm hover:bg-sky-600'>Delete</button>
+            </Table.Cell>
+          </Table.Row>
+         </Table.Body>) 
+        }
+
+      </Table>
+    </div>
   )
 }
 
